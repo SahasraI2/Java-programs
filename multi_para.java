91b578ac3e49905638e1e1689c3237733cc17062
SahasraI2 <153825867+SahasraI2@users.noreply.github.com>
2024-08-29 18:05:21 +0530

    Add files via upload

---
+++ multi_para.java
@@ -0,0 +1,10 @@
+public class multi_para{
+	public static int mul(int x, int y){
+		return x*y;
+	}
+	public static void main(String[] args){
+		int result;
+		result= mul(2,3);
+		System.out.println("product=" + result);
+	}
+}
---
+++ standard_methods.java
@@ -0,0 +1,11 @@
+public class standard_methods{
+	public static void main(String[] args){
+		int x=2;
+		String s = "sahasra";
+		String v = "HELLO";
+		System.out.println(java.lang.Math.sqrt(x));
+		System.out.println(s.toUpperCase());
+		System.out.println(v.toLowerCase());
+		System.out.println(s.length());
+	}
+}
